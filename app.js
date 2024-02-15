398e218d160b7b83452ebf45cd1b11a52c395c13
Clieford13 <cliefordjayrule@gmail.com>
2024-02-15 23:37:20 +0800

    newsssss

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ data.products.forEach(Product => {
     <h5 class="card-title">${Product.Product_Name}</h5>
     <p class="card-text">${Product.Product_Description}</p>
     <p class="card-text">Price: $${Product.Product_Price.toFixed(2)}</p>
-    <p class="card-text"><small class="text-body-secondary">Date Added: ${Product.Product_Date_Added}</small></p>
-    <p class="card-text"><small class="text-body-secondary">Expiration Date: ${Product.Product_Expiration_Date}</small></p>
+    <p class="card-text"><small class="text-body-secondary">Date Added: ${Product.Product_DateAdded}</small></p>
+    <p class="card-text"><small class="text-body-secondary">Expiration Date: ${Product.Product_ExpirationDate}</small></p>
     <button class="btn btn-primary add-to-cart" data-product="${Product.Product_Name}">Add to Cart</button>
   </div>
 `;
@@ -32,10 +32,10 @@ data.products.forEach(Product => {
   ProductCardsContainer.addEventListener("click", function(event) {
       if (event.target.classList.contains("add-to-cart")) {
           const ProductName = event.target.getAttribute("data-product");
-          if (ProductList[ProductName]) {
-              ProductList[ProductName]++;
+          if (Listing[ProductName]) {
+              Listing[ProductName]++;
           } else {
-              ProductList[ProductName] = 1;
+              Listing[ProductName] = 1;
           }
           updateProductListDisplay();
         }
