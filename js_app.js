eeaaa3620672797779b1ca996a2fa0ba72a9e102
Clieford13 <cliefordjayrule@gmail.com>
2024-02-15 21:23:20 +0800

    jssshhhh

--- js_app.js
+++ js_app.js
@@ -1,6 +1,9 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    const PRODUCTSElement = document.getElementById("PRODUCTS");
-    data.forEach(roduct => {
+    const ListingElement = document.getElementById("Listing");
+    data.forEach(product => {
       const productItem = document.createElement("div");
+      // Adding Bootstrap grid classes
+      productItem.classList.add('col-md-4', 'mb-4'); 
+      // Initialize click counter for each product
