07c0ebe0f67bbd70afbb3527e626d49b230216aa
Clieford13 <cliefordjayrule@gmail.com>
2024-02-15 21:20:10 +0800

    js

--- js_app.js
+++ js_app.js
@@ -1,3 +1,6 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
+    const PRODUCTSElement = document.getElementById("PRODUCTS");
+    data.forEach(roduct => {
+      const productItem = document.createElement("div");
